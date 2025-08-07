9a7e263caae16dca7309c7c855eea9ea675a8ecb
DuyetBKU <khongcostenn@gmail.com>
2025-08-07 16:36:14 +0700

    Update taylor.m

--- Lesson3/taylor.m
+++ Lesson3/taylor.m
@@ -1,23 +1,31 @@
 function taylor
 syms x
-f=input('nhap f(x)= ');
-x0=input('nhap x0= ');
-a=input('nhap so bac cua khai trien taylor: ');
-k=0;
-n=1;
-taylor=(subs(diff(f,k),x,x0)*(x-x0)^k)/n;
-taylor=double(taylor);
-if abs(taylor)<0.000001
-    taylor=0;
+f = input('Enter function f(x) = ');
+x0 = input('Enter the expansion point x0 = ');
+a = input('Enter the degree of the Taylor expansion: ');
+
+% Initialize variables
+k = 0;
+n = 1;
+taylor = (subs(diff(f, k), x, x0) * (x - x0)^k) / n;
+taylor = double(taylor);
+
+% Remove very small values (floating point tolerance)
+if abs(taylor) < 1e-6
+    taylor = 0;
 end
-k=1;
-while k<=a
-    n=n*k;     %tinh giai thua (k!)
-    if abs(subs(diff(f,k),x,x0))>0.0000001
-      taylor=taylor+((subs(diff(f,k),x,x0)*(x-x0)^k))/n;
+
+% Compute the Taylor expansion up to degree a
+k = 1;
+while k <= a
+    n = n * k;  % Compute factorial k!
+    term = subs(diff(f, k), x, x0);
+    if abs(term) > 1e-7
+        taylor = taylor + (term * (x - x0)^k) / n;
     end
-      k=k+1;
+    k = k + 1;
 end
-disp('khai trien taylor cua f la')
+
+disp('The Taylor expansion of f is:')
 disp(taylor)
 end
